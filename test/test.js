12e3e145148038e393d260d0b21db137ad646d12
Harry Stevens <harryjosephstevens@gmail.com>
2019-06-05 15:34:42 -0400

    Write basic test

--- test/test.js
+++ test/test.js
@@ -6,8 +6,47 @@ console.log(path.join(__dirname, "test.html"));
 console.log(path.resolve(__dirname, "test.html"));
 
 tape("$request requests some HTML and lets you use jQuery as the selection engine", function(test){
-  $request(path.resolve(__dirname, "test.html"), $ => {
-    console.log($("body").html());
+  $request("https://raw.githubusercontent.com/HarryStevens/jquery-scrape/master/test/test.html", function($){
+    test.equal($("h1").text().trim(), "Hello");
+    test.equal($("div").html(), "world!");
+
+    $("tr").each(function(rowIndex, row){
+
+      if (rowIndex === 0){
+        test.equal(rowIndex, 0);
+
+        $(row).find("td").each(function(cellIndex, cell){
+          const cellText = $(cell).text().trim();
+
+          if (cellIndex === 0){
+            test.equal(cellText, "1");
+          }
+
+          else {
+            test.equal(cellText, "Foo");
+          }
+
+        });
+      }
+
+      else {
+        test.equal(rowIndex, 1);
+
+        $(row).find("td").each(function(cellIndex, cell){
+          const cellText = $(cell).text().trim();
+
+          if (cellIndex === 0){
+            test.equal(cellText, "2");
+          }
+
+          else {
+            test.equal(cellText, "Bar");
+          }
+
+        });        
+      }
+    });
+
+    test.end();
   });
-  test.end();
 });
